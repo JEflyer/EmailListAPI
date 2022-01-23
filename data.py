b4a808793e809c3302f60e520c0eab7b7a361b30
Jonny <email@email.com>
2022-01-23 18:32:15 +0000

    first commit

---
+++ data.py
@@ -0,0 +1,7 @@
+class Email:
+    def __init__(self, firstName, lastName, email, number):
+        self.firstName = firstName
+        self.lastName = lastName
+        self.email = email
+        self.number = number
+        
---
+++ db.py
@@ -0,0 +1,27 @@
+import sqlite3
+from data import Email
+
+
+connection = sqlite3.connect(
+    ":memory:"
+)  # instead of :memory: use DbName.db in production
+
+cursor = connection.cursor()
+
+
+def createTable():
+    cursor.execute(
+        """CREATE TABLE EmailList (id INTEGER AUTOINCREMENT, firstName text, lastName text, email text, number text)"""
+    )
+
+def addEmail(Email):
+    cursor.execute(
+        """INSERT INTO EmailList VALUES (?,?,?,?)""",
+        (
+            Email.firstName,
+            Email.lastName,
+            Email.email,
+            Email.number
+        )
+    )
+
